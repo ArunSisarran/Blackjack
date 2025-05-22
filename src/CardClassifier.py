fcd19836deb389c714fdadd1d056d27352ba9af3
Arun Sisarran <arunsisarran@gmail.com>
2025-05-22 15:11:47 -0400

    first model

--- src/CardClassifier.py
+++ src/CardClassifier.py
@@ -7,18 +7,131 @@ import torchvision.transforms as transforms
 from torchvision.datasets import ImageFolder
 import timm
 
-import matplotlib.pyplot as plt # For data viz
+import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 import sys
+from tqdm import tqdm
 
 
 class PlayingCardDataset(Dataset):
     def __init__(self, data_dir, transform=None):
-        pass
+        self.data = ImageFolder(data_dir, transform=transform)
 
     def __len__(self):
-        pass
+        return len(self.data)
 
     def __getitem__(self, index):
-        return
+        return self.data[index]
+
+    @property
+    def classes(self):
+        return self.data.classes
+
+
+dataset = PlayingCardDataset(data_dir='../train/')
+image, label = dataset[6000]
+
+data_dir = '../train/'
+target_to_class = {v: k for k, v in ImageFolder(data_dir).class_to_idx.items()}
+
+transform = transforms.Compose([
+    transforms.Resize((128, 128)),
+    transforms.ToTensor(),
+])
+
+dataset = PlayingCardDataset(data_dir, transform)
+image, label = dataset[100]
+
+for image, label in dataset:
+    break
+
+dataloader = DataLoader(dataset, batch_size=32, shuffle=True)
+
+for images, labels in dataloader:
+    break
+
+
+class SimpleCardClassifier(nn.Module):
+    def __init__(self, num_classes=53):
+        super(SimpleCardClassifier, self).__init__()
+
+        self.base_model = timm.create_model('efficientnet_b0', pretrained=True)
+        self.features = nn.Sequential(*list(self.base_model.children())[:-1])
+
+        enet_out_size = 1280
+
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(enet_out_size, num_classes)
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        output = self.classifier(x)
+        return output
+
+
+train_folder = '../train/'
+valid_folder = '../valid/'
+test_folder = '../test/'
+
+train_dataset = PlayingCardDataset(train_folder, transform=transform)
+val_dataset = PlayingCardDataset(valid_folder, transform=transform)
+test_dataset = PlayingCardDataset(test_folder, transform=transform)
+
+train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
+val_loader = DataLoader(val_dataset, batch_size=32, shuffle=False)
+test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False)
+
+num_epochs = 5
+train_losses, val_losses = [], []
+
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+model = SimpleCardClassifier(num_classes=53)
+model.to(device)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+for epoch in range(num_epochs):
+
+    # Training phase
+    model.train()
+    running_loss = 0.0
+    for images, labels in tqdm(train_loader, desc='Training loop'):
+
+        # Move inputs and labels to the device
+        images, labels = images.to(device), labels.to(device)
+        optimizer.zero_grad()
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+        running_loss += loss.item() * labels.size(0)
+
+    train_loss = running_loss / len(train_loader.dataset)
+    train_losses.append(train_loss)
+
+    # Validation phase
+    model.eval()
+    running_loss = 0.0
+    with torch.no_grad():
+        for images, labels in tqdm(val_loader, desc='Validation loop'):
+
+            # Move inputs and labels to the device
+            images, labels = images.to(device), labels.to(device)
+            outputs = model(images)
+            loss = criterion(outputs, labels)
+            running_loss += loss.item() * labels.size(0)
+
+    val_loss = running_loss / len(val_loader.dataset)
+    val_losses.append(val_loss)
+    print(f"Epoch {epoch+1}/{num_epochs} - Train loss: {train_loss}, Validation loss: {val_loss}")
+
+plt.plot(train_losses, label='Training loss')
+plt.plot(val_losses, label='Validation loss')
+plt.legend()
+plt.title("Loss over epochs")
+plt.show()
